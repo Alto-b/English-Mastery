541238aed858a5902b13914de337116cba0cf909
Alto b <altob282@gmail.com>
2024-09-25 15:32:45 +0530

    hostname for writing1 generate updated

--- lib/infrastructure/writing1_repo.dart
+++ lib/infrastructure/writing1_repo.dart
@@ -1,13 +1,14 @@
 import 'dart:convert';
 import 'package:english_mastery/domain/writing1_task_model.dart';
 import 'package:english_mastery/env.dart';
+import 'package:english_mastery/urls.dart';
 import 'package:http/http.dart' as http;
 
 class Writing1Repo {
   Future<Writing1GenerateTaskModel?> writing1_generate_task() async {
     try {
       final response = await http.get(
-        Uri.parse("${Env.host}writing1/generate_task/"),
+        Uri.parse("${Env.host}${URLs.writing1_generate_task}"),
         headers: {
           'Content-Type': 'application/json',
         },
