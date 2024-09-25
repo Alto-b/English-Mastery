37718ca0f72455e60e3ee637db6f448bd19f23c0
Alto b <altob282@gmail.com>
2024-09-25 15:28:19 +0530

    urls updated

---
+++ lib/urls.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: constant_identifier_names
+
+class URLs {
+  URLs._privateConstructor();
+
+  // writing1
+  static const writing1_generate_task = 'writing1/generate_task/';
+  static const writing1_check_grammar = 'writing1/';
+  static const writing1_evaluation = 'writing1/';
+
+  // writing2
+  static const writing2_generate_task = 'writing1/generate_task/';
+  static const writing2_check_grammar = 'writing2/';
+  static const writing2_evaluation = 'writing2/';
+}
