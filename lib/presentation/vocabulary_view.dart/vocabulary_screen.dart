2223d078701ac6333d4028aa05212b524d2cc3f9
Alto b <altob282@gmail.com>
2024-11-04 16:06:56 +0530

    loader added for drop down loading

--- lib/presentation/vocabulary_view.dart/vocabulary_screen.dart
+++ lib/presentation/vocabulary_view.dart/vocabulary_screen.dart
@@ -4,8 +4,10 @@ import 'package:english_mastery/presentation/vocabulary_view.dart/widgets/error_
 import 'package:english_mastery/presentation/vocabulary_view.dart/widgets/multiple_choice_widget.dart';
 import 'package:english_mastery/presentation/vocabulary_view.dart/widgets/sentence_completion_widget.dart';
 import 'package:english_mastery/presentation/vocabulary_view.dart/widgets/synonyms_antonyms_widget.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gap/gap.dart';
 import 'package:lottie/lottie.dart';
 
 class VocabularyScreen extends StatelessWidget {
@@ -55,55 +57,63 @@ class VocabularyScreen extends StatelessWidget {
         children: [
           Padding(
             padding: const EdgeInsets.all(16.0),
-            child: Row(
-              children: [
-                const Text(
-                  "Select category:",
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
-                ),
-                const SizedBox(width: 10),
-                Expanded(
-                  child: ValueListenableBuilder<String>(
-                    valueListenable: selectedValue,
-                    builder: (context, value, _) {
-                      return Container(
-                        padding: const EdgeInsets.symmetric(horizontal: 16),
-                        decoration: BoxDecoration(
-                          color: Colors.grey.shade200,
-                          borderRadius: BorderRadius.circular(8),
-                          border: Border.all(color: Colors.grey.shade400),
-                        ),
-                        child: DropdownButtonHideUnderline(
-                          child: DropdownButton<String>(
-                            value: value,
-                            items: levels.map((String level) {
-                              return DropdownMenuItem<String>(
-                                value: level,
-                                child: Text(
-                                  level,
-                                  style: const TextStyle(fontSize: 16),
-                                ),
-                              );
-                            }).toList(),
-                            onChanged: (String? newValue) {
-                              if (newValue != null) {
-                                selectedValue.value = newValue;
-                                handleSelection(context, newValue);
-                              }
-                            },
-                            dropdownColor: Colors.white,
-                            icon: const Icon(
-                              Icons.arrow_drop_down,
-                              color: Colors.blueAccent,
-                              size: 28,
+            child: BlocBuilder<VocabularyBloc, VocabularyState>(
+              builder: (context, state) {
+                if (state is VocabularyLoadingState) {
+                  return SizedBox.shrink();
+                }
+                return Row(
+                  children: [
+                    const Text(
+                      "Select category:",
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                    ),
+                    const SizedBox(width: 10),
+                    Expanded(
+                      child: ValueListenableBuilder<String>(
+                        valueListenable: selectedValue,
+                        builder: (context, value, _) {
+                          return Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 16),
+                            decoration: BoxDecoration(
+                              color: Colors.grey.shade200,
+                              borderRadius: BorderRadius.circular(8),
+                              border: Border.all(color: Colors.grey.shade400),
                             ),
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                ),
-              ],
+                            child: DropdownButtonHideUnderline(
+                              child: DropdownButton<String>(
+                                value: value,
+                                items: levels.map((String level) {
+                                  return DropdownMenuItem<String>(
+                                    value: level,
+                                    child: Text(
+                                      level,
+                                      style: const TextStyle(fontSize: 16),
+                                    ),
+                                  );
+                                }).toList(),
+                                onChanged: (String? newValue) {
+                                  if (newValue != null) {
+                                    selectedValue.value = newValue;
+                                    handleSelection(context, newValue);
+                                  }
+                                },
+                                dropdownColor: Colors.white,
+                                icon: const Icon(
+                                  Icons.arrow_drop_down,
+                                  color: Colors.blueAccent,
+                                  size: 28,
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                );
+              },
             ),
           ),
           Expanded(
@@ -126,7 +136,21 @@ class VocabularyScreen extends StatelessWidget {
                 } else if (state is VocabularyErrorCorrectionState) {
                   return ErrorCorrectionView(state: state);
                 } else if (state is VocabularyLoadingState) {
-                  return Center(child: Text("Loading..."));
+                  return const Center(
+                      child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      CupertinoActivityIndicator(
+                        color: Colors.lightBlue,
+                        radius: 15,
+                      ),
+                      Gap(15),
+                      Text(
+                        "Loading...",
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ],
+                  ));
                 } else if (state is VocabularyMultipleChoicState) {
                   return MultipleChoiceView(state: state);
                 } else if (state is VocabularySynonymsAntonymsState) {
